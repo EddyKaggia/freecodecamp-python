8c39eb30d297801e952254b0256781c4ed22b0ea
EddyKaggia <kagzcodes@gmail.com>
2024-10-15 20:14:57 -0700

    feat: python string fundamentals

---
+++ Fundamentals/main.py
@@ -0,0 +1,13 @@
+# BITWISE OPERATORS
+# -------------------
+# & -> performs binary AND
+# | -> perfOrms binary OR
+# ^ -> performs a binary XOR operation
+# ~ -> performs a binary NOT operation
+# << -> shift left operation
+# >> -> shift right operation
+
+# IS AND IN OPERATORS
+# ---------------------
+# is -> identity operator, and compares two objects. If they are the same it returns true
+# in -> membership operator, and checks if a value exists in a sequence like a list
